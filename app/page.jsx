b70b0f27a90d08d601734faf8c98aa29ab6dcf0f
Blue-Onion <rawatadityasingh963@gmail.com>
2025-09-19 17:12:38 +0530

    Start

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="relative container mx-auto">
+      <section className="py-32 text-center">
+        <div className='space-y-6 mx-auto'>
+                <h1 className='text-4xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title'>Your Story, Tessera Seamlessly Animated
+
+                </h1>
+                <p className='mx-auto max-w-xs md:max-w-2xl text-muted-foreground md:text-xl'>
+                    Bring your ideas to life through elegant animation, seamless storytelling, and intelligent design.
+                </p>
+                <div className="flex items-center flex-col md:flex-row justify-center gap-4">
+                  <Button>Start Now</Button>
+                  <Button>Learn More</Button>
+                </div>
+            </div>
+      </section>
+    </div>
+  );
+}
